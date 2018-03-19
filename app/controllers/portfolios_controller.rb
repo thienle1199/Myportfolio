c2ad0343d7f91b96b6536aafebd5716deb9581a9
Thiienle <thienle.luong@gmail.com>
2018-03-19 22:09:25 +0700

    Integrated new and create action for Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,18 @@
 class PortfoliosController < ApplicationController
   def index
-    @portfolio_items = Portfolio.all 
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    if @portfolio_item.save
+      redirect_to portfolios_path, notice: "Your portfolio is now live"
+    else
+      render 'new'
+    end
   end
 end
