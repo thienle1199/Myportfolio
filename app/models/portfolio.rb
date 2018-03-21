3cbaaa5fa8d93fc630590516408e29d0a3bb4c81
Thiienle <thienle.luong@gmail.com>
2018-03-21 14:37:27 +0700

    Integrated nested attributes for Portfolio omodel

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,13 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: proc { |attrs| attrs['name'].blank?}
   include Placeholder
   # implement custom scope
   def self.anglular
     where(subtitle: 'Anglular')
   end
-  scope :ruby_on_rails, lambda { where(:subtitle => 'Ruby on Rails') }
+  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 
   validates_presence_of :title, :body, :main_image, :thumb_image
   after_initialize :set_defaults
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
-)
+  )
 end
-10.times do |blog|
+10.times do
   Blog.create!(
     title: Faker::SiliconValley.invention,
     body: Faker::SiliconValley.quote,
-    topic_id: Topic.last.id)
+    topic_id: Topic.last.id
+  )
 end
 
 puts '10 blog posts created'
-5.times do |skill|
+5.times do
   Skill.create!(
     title: Faker::ProgrammingLanguage.name,
     percent_utilized: rand(30..100)
@@ -19,15 +20,15 @@ puts '10 blog posts created'
 end
 puts '5 times skill created'
 
-9.times do |portfolio_item|
+9.times do
   Portfolio.create!(
     title: Faker::Job.title,
     subtitle: Faker::Job.field,
     body: Faker::SiliconValley.quote,
-    main_image: Faker::Placeholdit.image("600x400"),
-    thumb_image: Faker::Placeholdit.image("350x150")
+    main_image: Faker::Placeholdit.image('600x400'),
+    thumb_image: Faker::Placeholdit.image('350x150')
   )
-puts '9 portfolio items created'
+  puts '9 portfolio items created'
 end
 3.times do |tech|
   Technology.create!(
