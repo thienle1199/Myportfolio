2981104908c444d42f3ab432d5f1ec2c4b76c6a3
Thiienle <thienle.luong@gmail.com>
2018-03-24 09:12:14 +0700

    Implemented view helper for session source dunctionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     base_title = 'Full-Stack Ruby on Rails web Developer'
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: 'source-greeting')
+    end
+  end
 end
