7a75b8027e97471961f5a9fb21b4fc9b2cf0c9b8
Thiienle <thienle.luong@gmail.com>
2018-03-31 12:24:51 +0700

    updated style for master layout file

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[show edit update destroy toggle_status]
   authorize_resource
+  layout 'blog'
   # GET /blogs
   # GET /blogs.json
   def index
